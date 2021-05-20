4885067061083c3831d61a651a1827daec533ef7
Sanshone <sanshonekrashe@gmail.com>
2021-05-21 08:01:24 +1000

    stuff

--- lib/gen.sh
+++ lib/gen.sh
@@ -3,10 +3,7 @@
 cp -f lib/start.html index.html
 n=1
 for f in md/*
-	do pandoc -s $f --template lib/post.html --toc-depth=2 -o out/$n.html
-	cat out/$n.html >> index.html
-    n = $((n+1))
-done
+do pandoc -s $f --template lib/post.html -o out/$n.html && cat out/$n.html >> index.html && n=$((n+1))
 done
 
 cat lib/end.html >> index.html
