1c4c174d43d7f461283b0c16bd7b1ad3f4489bb4
skrshe <sanshonekrashe@gmail.com>
2021-06-09 21:40:27 +1000

    change generation cycle and fix posts

--- lib/gen.sh
+++ lib/gen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cp -f lib/start.html index.html
-n=1
+
 for f in md/*
-do pandoc -s $f --template lib/post.html -o out/$n.html && cat out/$n.html >> index.html && n=$((n+1))
+do pandoc -s $f --template lib/post.html -o post_cache.html && cat post_cache.html >> index.html
 done
 
 cat lib/end.html >> index.html
